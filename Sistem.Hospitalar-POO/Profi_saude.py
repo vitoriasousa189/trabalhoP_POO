7a72ddfa0c3415c1d64ee0e35ed6fcd20de58850
Vivii <vitoriasous826@gmail.com>
2026-08-25 22:20:05 -0300

    Add files via upload

---
+++ Sistem.Hospitalar-POO/Profi_saude.py
@@ -0,0 +1,6 @@
+class ProfissionalSaude: 
+    def __init__(self, nome, registro):
+        self.nome = nome 
+        self.registro = registro
+    def trabalhar(self):
+        return "trabalhando no hospital..."
---
+++ Sistem.Hospitalar-POO/hospital.py
@@ -0,0 +1,4 @@
+class Hospital:
+    def __init__(self, nome, equipe):
+        self.nome = nome
+        self.equipe = equipe
